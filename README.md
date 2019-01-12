16a34fa53c53c429581aa6ab8896726dfdd935c6
Matthew <mdong@berkeley.edu>
2019-01-12 15:34:22 -0800

    update server: base64 conversion

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cardboard
 
+1. User clicks on RPI 
+1. Mouseclick coordinates sent through websocket
+1. Click on actual device
+1. Screenshot of result sent back to RPI
--- server.py
+++ server.py
@@ -3,11 +3,9 @@ import socket
 import websockets
 import asyncio
 from pymouse import PyMouse
-import json
 import time
 import pyscreenshot as ImageGrab
 from PIL import Image
-import requests
 import base64
 from io import BytesIO
 
@@ -37,74 +35,29 @@ sock.listen(1)
 
 width = None
 height = None
-
+image_base64 = ''
 
 async def hello(websocket, path):
-    coords = await websocket.recv()
-    print(f"< {coords}")
-    dimension = coords[1:-1].split(',')
-    print('[INFO]: dimension')
-    m = PyMouse()
-    im = ImageGrab.grab()
-    im.save('screenshot.png')
-    im.show()
-    x_dim, y_dim = m.screen_size()
+	coords = await websocket.recv()
+	print(f"< {coords}")
+	dimension = coords[1:-1].split(',')
+	print('[INFO]: dimension')
+	m = PyMouse()
+	x_dim, y_dim = m.screen_size()
+	m.click(x_dim * float(dimension[0]), y_dim * float(dimension[1]))
 
-    m.click(x_dim * float(dimension[0]), y_dim * float(dimension[1]))
+	img = ImageGrab.grab()
+	img.save('screenshot.png')
+	time.sleep(1)
+	# img.show()
+	buffered = BytesIO()
+	img.save(buffered, format="PNG")
+	image_base64 = base64.b64encode(buffered.getvalue())
+	
+	payload = {"base64": image_base64.decode('utf-8')}
+	return payload
 
 start_server = websockets.serve(hello, '10.30.3.126', 23456)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
-
-
-# while True:  
-#   # wait for a connection
-#   # print ('waiting for a connection')
-
-#   # try:
-#       # show who connected to us
-#   print('Ready to accept now...') 
-#   connection, client_address = sock.accept()
-#   print ('connection from', client_address)
-#   # receive the data in small chunks and print it
-#   time.sleep(1)
-#   print ("1")
-
-#   # data = connection.recv(4096)
-#   # if (data != type(byte)):
-#   while 1:
-#       data = connection.recv(8000)
-#       if not data: break
-#       print(data)
-#   # print (data)
-#   # except:
-#       # print ("REEEEEEEE")
-
-#       # print (data.decode('utf-8'))
-#       # if width is None:
-#       #   # output received data
-#       #   print ("width: %s" % data)
-#       #   width = float(data)
-#       # elif height is None:
-#       #   print("height: %s" % data)
-#       #   height = float(data.decode())
-#       # else:
-#       #   # no more data -- quit the loop
-#       #   print ("no more data.")
-#       #   break
-
-#   # finally:
-#       # Clean up the connection
-#       # print(width)
-#       # print(height)
-
-#       # m = PyMouse()
-#       # k = PyKeyboard()
-#       # x_dim, y_dim = m.screen_size()
-
-#       # m.click(x_dim * width, y_dim * height, 1)
-#       # # print('done')
-
-#       # connection.close()
-# socket.close()
