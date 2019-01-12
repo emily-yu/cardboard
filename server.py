b3e388bd404bc67361a4c3a472a6eb1b5c4df17e
ey1031 <eyudeveloper@gmail.com>
2019-01-12 13:34:14 -0800

    ahhHHHHHhh sockets not working

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 import socket
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
+import json
+import time
 
 # create TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,40 +31,47 @@ sock.listen(1)
 width = None
 height = None
 
+
 while True:  
 	# wait for a connection
 	# print ('waiting for a connection')
-	connection, client_address = sock.accept()
 
-	try:
+	# try:
 		# show who connected to us
-		print ('connection from', client_address)
+	connection, client_address = sock.accept()
+	print ('connection from', client_address)
+	# receive the data in small chunks and print it
+	time.sleep(1)
+	print ("1")
+	data = connection.recv(4096)
+	# if (data != type(byte)):
+	print (data)
+	# except:
+		# print ("REEEEEEEE")
 
-		# receive the data in small chunks and print it
-		while True:
-			data = connection.recv(64)
-			if width is None:
-				# output received data
-				print ("width: %s" % data)
-				width = float(data)
-			elif height is None:
-				print("height: %s" % data)
-				height = float(data)
-			else:
-				# no more data -- quit the loop
-				print ("no more data.")
-				break
+		# print (data.decode('utf-8'))
+		# if width is None:
+		# 	# output received data
+		# 	print ("width: %s" % data)
+		# 	width = float(data)
+		# elif height is None:
+		# 	print("height: %s" % data)
+		# 	height = float(data.decode())
+		# else:
+		# 	# no more data -- quit the loop
+		# 	print ("no more data.")
+		# 	break
 
-	finally:
+	# finally:
 		# Clean up the connection
 		# print(width)
 		# print(height)
 
-		m = PyMouse()
-		k = PyKeyboard()
-		x_dim, y_dim = m.screen_size()
+		# m = PyMouse()
+		# k = PyKeyboard()
+		# x_dim, y_dim = m.screen_size()
 
-		m.click(x_dim * width, y_dim * height, 1)
-		# print('done')
+		# m.click(x_dim * width, y_dim * height, 1)
+		# # print('done')
 
-		connection.close()
+		# connection.close()
